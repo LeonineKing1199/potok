97f094ae1eb56a84fd8f4208ec67f2fc184a8b02
LeonineKing1199 <christian.mazakas@gmail.com>
2021-11-02 11:59:00 -0700

    Update hpack encoding to work with Asio's `DynamicBuffer_V2`

--- tests/hpack_integer_representation.cpp
+++ tests/hpack_integer_representation.cpp
@@ -3,18 +3,23 @@
 
 #include <potok/span.hpp>
 
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/buffers_iterator.hpp>
+
 #include <boost/assert.hpp>
 #include <boost/throw_exception.hpp>
 
-#include <stdexcept>
 #include <cstdint>
 #include <limits>
+#include <stdexcept>
+#include <vector>
 
 namespace potok {
 
-using u8  = std::uint8_t;
-using u32 = std::uint32_t;
-using u64 = std::uint64_t;
+using u8    = std::uint8_t;
+using u32   = std::uint32_t;
+using u64   = std::uint64_t;
+using usize = std::size_t;
 
 namespace hpack {
 
@@ -46,9 +51,7 @@ constexpr auto get_num_required_octets(u64 const x, u8 const num_prefix_bits) ->
 {
   u64 const max_prefix_value = (u64{1} << num_prefix_bits) - 1;
 
-  if (x < max_prefix_value) {
-    return 1;
-  }
+  if (x < max_prefix_value) { return 1; }
 
   auto I = x - max_prefix_value;
 
@@ -65,40 +68,53 @@ static_assert(get_num_required_octets(14, 5) == 1);
 static_assert(get_num_required_octets(35, 5) == 2);
 static_assert(get_num_required_octets(std::numeric_limits<u64>::max(), 1) == 11);
 
-constexpr auto encode_integer(u8 const num_prefix_bits, u64 const x, potok::span<u8> buf) -> span<u8>
+template <class DynamicBuffer_V2>
+auto encode_integer(u8 const         num_prefix_bits,    //
+                    u64 const        x,                  //
+                    DynamicBuffer_V2 buf,                //
+                    usize const      pos) -> usize
 {
   BOOST_ASSERT(num_prefix_bits >= 1 && num_prefix_bits <= 8);
-  BOOST_ASSERT(!buf.empty());
 
   auto const num_required_octets = get_num_required_octets(x, num_prefix_bits);
-  if (buf.size() < num_required_octets) {
-    BOOST_THROW_EXCEPTION(std::runtime_error("buf does not have enough capacity"));
+
+  buf.grow(num_required_octets);
+  auto mutable_buf_seq = buf.data(pos, num_required_octets);
+
+  if (boost::asio::buffer_size(mutable_buf_seq) < num_required_octets) {
+    boost::throw_exception(std::runtime_error("Unable to extend underlying DynamicBuffer_V2"));
   }
 
   u64 const max_prefix_value = (u64{1} << num_prefix_bits) - 1;
 
-  auto* out = buf.data();
+  auto out = boost::asio::buffers_begin(mutable_buf_seq);
+
+  auto bytes_written = usize{0};
 
   *out &= ~max_prefix_value;
 
   if (x < max_prefix_value) {
     *out++ |= x;
-    return buf.subspan(num_required_octets);
+    ++bytes_written;
+    return bytes_written;
   }
 
   *out++ |= max_prefix_value;
+  ++bytes_written;
 
   auto I = x - max_prefix_value;
   while (I >= 128) {
     auto const v = (I % 128 + 128);
 
     *out++ = v;
+    ++bytes_written;
     I /= 128;
   }
 
   *out++ = I;
+  ++bytes_written;
 
-  return buf.subspan(num_required_octets);
+  return bytes_written;
 }
 
 }    // namespace hpack
@@ -115,23 +131,21 @@ TEST_CASE("C.1.1. Example 1: Encoding 10 Using a 5-Bit Prefix")
   auto const value           = 10;
 
   {
-    auto storage = u8{0b11111111};
+    auto storage = std::vector<u8>{0b11100000};
+    auto buf     = boost::asio::dynamic_vector_buffer(storage);
+    auto pos     = 0;
 
-    auto buf = potok::span<u8>(&storage, 1);
-
-    potok::hpack::encode_integer(num_prefix_bits, value, buf);
-
-    REQUIRE(storage == u8{0b11101010});
+    REQUIRE(1 == potok::hpack::encode_integer(num_prefix_bits, value, buf, pos));
+    REQUIRE(storage[0] == u8{0b11101010});
   }
 
   {
-    auto storage = u8{0b00011111};
+    auto storage = std::vector<u8>{0b00011111};
+    auto buf     = boost::asio::dynamic_vector_buffer(storage);
+    auto pos     = 0;
 
-    auto buf = potok::span<u8>(&storage, 1);
-
-    potok::hpack::encode_integer(num_prefix_bits, value, buf);
-
-    REQUIRE(storage == u8{0b00001010});
+    REQUIRE(1 == potok::hpack::encode_integer(num_prefix_bits, value, buf, pos));
+    REQUIRE(storage[0] == u8{0b00001010});
   }
 }
 
@@ -143,19 +157,22 @@ TEST_CASE("C.1.2. Example 2: Encoding 1337 Using a 5-Bit Prefix")
   auto const value           = 1337;
 
   {
-    u8 storage[] = {0, 0, 0};
+    auto storage = std::vector<u8>{};
+    auto buf     = boost::asio::dynamic_vector_buffer(storage);
+    auto pos     = 0;
 
-    potok::hpack::encode_integer(num_prefix_bits, value, storage);
-
+    REQUIRE(3 == potok::hpack::encode_integer(num_prefix_bits, value, buf, pos));
     CHECK(storage[0] == 0b00011111);
     CHECK(storage[1] == 0b10011010);
     CHECK(storage[2] == 0b00001010);
   }
 
   {
-    u8 storage[] = {0b11100000, 1, 7};
+    auto storage = std::vector<u8>{0b11100000, 1, 7};
+    auto buf     = boost::asio::dynamic_vector_buffer(storage);
+    auto pos     = 0;
 
-    potok::hpack::encode_integer(num_prefix_bits, value, storage);
+    REQUIRE(3 == potok::hpack::encode_integer(num_prefix_bits, value, buf, pos));
 
     CHECK(storage[0] == 0b11111111);
     CHECK(storage[1] == 0b10011010);
@@ -171,33 +188,25 @@ TEST_CASE("C.1.3. Example 3: Encoding 42 Starting at an Octet Boundary")
     auto const num_prefix_bits = 8;
     auto const value           = 42;
 
-    auto storage = u8{0b10011001};
-    auto buf     = potok::span<u8>(&storage, 1);
+    auto storage = std::vector<u8>{0b10011001};
+    auto buf     = boost::asio::dynamic_vector_buffer(storage);
+    auto pos     = 0;
 
-    potok::hpack::encode_integer(num_prefix_bits, value, buf);
-
-    REQUIRE(storage == 0b00101010);
+    REQUIRE(1 == potok::hpack::encode_integer(num_prefix_bits, value, buf, pos));
+    REQUIRE(storage[0] == 0b00101010);
   }
 
   {
     auto const num_prefix_bits = 8;
     auto const value           = 325;
 
-    auto storage = u64{0b10011001};
-    auto buf     = potok::span<u8>(reinterpret_cast<u8*>(&storage), 4);
+    auto storage = std::vector<u8>{0b10011001};
+    auto buf     = boost::asio::dynamic_vector_buffer(storage);
+    auto pos     = 0;
 
-    potok::hpack::encode_integer(num_prefix_bits, value, buf);
+    REQUIRE(2 == potok::hpack::encode_integer(num_prefix_bits, value, buf, pos));
 
-    REQUIRE(buf[0] == 0b11111111);
-    REQUIRE(buf[1] == 0b01000110);
+    REQUIRE(storage[0] == 0b11111111);
+    REQUIRE(storage[1] == 0b01000110);
   }
-}
-
-TEST_CASE("Not enough space")
-{
-  auto const num_prefix_bits = 4;
-  auto const value           = u64{123423423};
-
-  u8 storage[] = {0x00};
-  REQUIRE_THROWS(potok::hpack::encode_integer(num_prefix_bits, value, storage));
 }
